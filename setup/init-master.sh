a1e812383c7755fc887a3f7226e503ce7d6c0549
c10t <c10t@fa.ke>
2018-06-22 21:38:51 +0900

    setup kubeadm and kubectl

--- setup/init-master.sh
+++ setup/init-master.sh
@@ -3,3 +3,14 @@ diff /etc/systemd/system/kubelet.service.d/10-kubeadm.conf 10-kubeadm.conf
 sudo mv -f 10-kubeadm.conf /etc/systemd/system/kubelet.service.d/
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
+
+# setup kubeadm
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=172.16.20.11 --service-cidr=10.244.0.0/16 > kubeadm-init-result.txt
+
+# setup kubectl
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# check if kubectl setup
+kubectl get node
